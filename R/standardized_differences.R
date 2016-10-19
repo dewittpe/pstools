377b0afc64a4f4a937a8611cc49ef114aec81a71
Peter DeWitt (latitude) <peter.dewitt@ucdenver.edu>
2016-10-18 23:16:34 -0600

    Added the basics for a print and plotting method of pstool_std_diffs objects.
    The printing method is a forest plot, re #2.

--- R/standardized_differences.R
+++ R/standardized_differences.R
@@ -66,9 +66,59 @@ standardized_differences.pstools_dstats <- function(x, ...) {
            y = std_diffs, 
            by = "key", 
            SIMPLIFY = FALSE)
+  names(out) <- c("unadj", "adj_IPW", "adj_ACE_Exposed", 
+                  "adj_ACE_Unexposed", "adj_ACE_MostInfo", "adj_ACE_MWP")
 
   attr(out, "continuous") <- attr(x, "continuous")
   class(out) <- c("pstools_std_diffs", class(out))
   out
 }
 
+frmt_pstools_std_diffs <- function(x, ...) {
+  mean_sd <- function(x, y) {
+    paste0(qwraps2::frmt(x), " (", qwraps2::frmt(y), ")")
+  }
+
+  percent <- function(x) {
+    paste0(qwraps2::frmt(x * 100, 1), "%")
+  }
+
+  x <- lapply(x, dplyr::mutate_, .dots = list("cnt" = ~ attr(x, "continuous")))
+
+  x <- 
+    mapply(function(dat, cnt) {
+             dat <- dplyr::rowwise(dat)
+             dat <- dplyr::transmute_(dat,
+                                      .dots = list( 
+                                                   key = ~ key,
+                                                   unexposed = ~ if (cnt) { mean_sd(mean.z0, sqrt(var.z0)) } else { percent(mean.z0) },
+                                                   exposed = ~ if (cnt) { mean_sd(mean.z1, sqrt(var.z1)) } else { percent(mean.z1) },
+                                                   std_diff = ~ paste0(qwraps2::frmt(std_diff, 2), "%")
+                                                   )
+                                      )
+             dat <- dplyr::ungroup(dat)
+                  },
+           dat = x, 
+           MoreArgs = list(attr(x, "continuous")), 
+           SIMPLIFY = FALSE)
+}
+
+#' @export
+print.pstools_std_diffs <- function(x, ...) {
+  x <- frmt_pstools_std_diffs(x, ...) 
+  print(x)
+}
+
+#' @export
+plot.pstools_std_diffs <- function(x, adj_methods = c("adj_IPW", "adj_ACE_Exposed", "adj_ACE_Unexposed", "adj_ACE_MostInfo", "adj_ACE_MWP"), ...) { 
+  dat <- dplyr::bind_rows(x, .id = "adjustment")
+  dat <- dplyr::filter_(dat, .dots = ~ adjustment %in% c("unadj", adj_methods))
+  dat$key <- factor(dat$key)
+  levels(dat$key) <- rev(levels(dat$key))
+
+  ggplot2::ggplot(dat) +
+  ggplot2::aes_string(x = "std_diff", y = "key", 
+                      color = "adjustment", shape = "adjustment") +
+  ggplot2::geom_point() +
+  ggplot2::geom_vline(xintercept = 0)
+}
